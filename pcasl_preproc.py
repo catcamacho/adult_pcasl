ca32390aa4b3351e88f478f47a705d1af902256e
catcamacho <m.catalina.camacho@gmail.com>
2016-12-25 13:26:53 -0600

    Add slicetiming

--- pcasl_preproc.py
+++ pcasl_preproc.py
@@ -1,14 +1,20 @@
 import os
 import numpy as np
 import nibabel as nib
-import nipy as nip
+from glob import glob
+import nipy as ni
 import nipype as npe
 from nipype.interfaces.nipy.preprocess import Trim
 trim = Trim()
+from nipype.interfaces import fsl
+st = fsl.SliceTimer()
 
-path = '/Users/catcamacho/Box Sync/CBFBIRN_practice/210-NART2'
+path = '/Users/catcamacho/Documents/asl_practice/210-NART2'
 imgFile = path + '/raw/asl.nii.gz'
 img = nib.load(imgFile)
+aslVolFile = path + '/aslVol.nii.gz'
+pdVolFile = path + '/pdVol.nii.gz'
+	
 
 # Determine file type and transform to NIFTI if need be
 
@@ -25,27 +31,29 @@ def getImgMeta(img):
 	return numVols
 
 # Separate the asl and pd volumes
-def splitASLvols(imgFile, path):
-	aslVolFile = path + '/aslVol.nii.gz'
-	pdVolFile = path + '/pdVol.nii.gz'
-	
+def splitASLvols(imgFile, aslVolFile, pdVolFile):	
 	trim.inputs.in_file = imgFile 
 	trim.inputs.out_file = aslVolFile
 	trim.inputs.end_index = 1
-	aslVol = trim.run()
+	trim.run()
 	
 	trim.inputs.out_file = pdVolFile
 	trim.inputs.end_index = 2
 	trim.inputs.begin_index = 1
-	pdVol = trim.run()
-	
-	return aslVolFile
-	return pdVolFile	
+	trim.run()
 
 # Slice-timing correction
-
-
+def sliceTimeASL(aslVolFile, path):
+	st.inputs.time_repetition = 4.674
+	st.inputs.slice_direction = 3
+	st.inputs.interleaved = False
+	st.inputs.in_file = aslVolFile
+	st.inputs.out_file = path + '/st_aslVol.nii.gz'
+	st.inputs.output_type = 'NIFTI_GZ'
+	st.run()
+	
 # NU correction
+
 
 # M0 Calculation
 # def M0calc(pdVol):
@@ -54,5 +62,6 @@ def splitASLvols(imgFile, path):
 
 # Run the defined functions
 getImgMeta(img)
-splitASLvols(imgFile, path)
+splitASLvols(imgFile, aslVolFile, pdVolFile)
+sliceTimeASL(aslVolFile, path)
 
