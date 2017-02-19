7cafdc45fe02aca639a17ca7b022c00a197bcf92
catcamacho <m.catalina.camacho@gmail.com>
2017-02-18 17:03:54 -0800

    update preproc

--- pcasl_preproc.py
+++ pcasl_preproc.py
@@ -10,7 +10,7 @@ from nipype.interfaces import fsl
 trim = Trim()
 st = fsl.SliceTimer()
 
-path = '/Users/catcamacho/Documents/asl_practice/210-NART2'
+path = '/Users/myelin/Documents/pcasl_processing/test_data_KO'
 img_file = path + '/raw/asl.nii.gz'
 asl_vol_file = path + '/aslVol.nii.gz'
 pd_vol_file = path + '/pdVol.nii.gz'
@@ -26,52 +26,53 @@ def data_type(img_file):
     pass
 
 
-def get_img_meta(img):
+def get_img_meta(img_data):
     """
     Collect file properties to be used as sanity checks down the line.
     """
-    header = img.header
-    dim = header.get_data_shape()
-    matrix = dim[0:2]
-    numslices = dim[2]
-    numvols = dim[3]
-    dimensions = {'matrix': matrix, 'numslices': numslices, 'numvols': numvols}
+	header = img_data.header
+	dim = header.get_data_shape()
+	matrix = dim[0:2]
+	numslices = dim[2]
+	numvols = dim[3]
+	dimensions = {'matrix': matrix, 'numslices': numslices, 'numvols': numvols}
 
-    print dimensions
-    return dimensions
+	print dimensions
+	return dimensions
 
 
 def split_asl_vols(img_file, asl_vol_file, pd_vol_file):
     """
     Read in the combo nifti and and split the ASL and PD Volumes.
     """
-    trim.inputs.in_file = img
-    trim.inputs.out_file = asl_vol_file
-    trim.inputs.end_index = 1
-    trim.run()
-    trim.inputs.out_file = pd_vol_file
-    trim.inputs.end_index = 2
-    trim.inputs.begin_index = 1
-    trim.run()
+	trim.inputs.in_file = img_file
+	trim.inputs.out_file = asl_vol_file
+	trim.inputs.end_index = 1
+	trim.run()
+	trim.inputs.out_file = pd_vol_file
+	trim.inputs.end_index = 2
+	trim.inputs.begin_index = 1
+	trim.run()
 
 
-def slicetime_asl(asl_vol_file, st_corr_vol_file):
+def slicetime_asl(in_file, out_file):
     """
     Correct slice timing using FSL-need to replace with a custom one.
     """
-    st.inputs.time_repetition = 4.674
-    st.inputs.slice_direction = 3
-    st.inputs.interleaved = False
-    st.inputs.in_file = asl_vol_file
-    st.inputs.out_file = st_corr_vol
-    st.inputs.output_type = 'NIFTI_GZ'
-    st.run()
+	st.inputs.time_repetition = 4.674
+	st.inputs.slice_direction = 3
+	st.inputs.interleaved = False
+	st.inputs.in_file = in_file
+	st.inputs.out_file = out_file
+	st.inputs.output_type = 'NIFTI_GZ'
+	st.run()
 
 
-def nonuniformity_correction_asl(st_corr_vol_file):
+def n3_correction(vol_file):
     """
-    Correct nonuniformity slice by slice low pass filtering via 3D FFT
+    Correct nonuniformity slice by slice using MINC N3 tools
     """
+    
     pass
 
 
@@ -105,13 +106,18 @@ def calculate_m0(pd_vol):
     Calculate M0 value from the PD volume.
     """
     pd = nib.load(pd_vol)
+    pass
+
+def plot_funct():
+
+    pass
 
 
 def main():
     get_img_meta(img)
     split_asl_vols(img_file, asl_vol_file, pd_vol_file)
     slicetime_asl(asl_vol_file, st_corr_vol_file)
-    nonuniformity_correction_asl(st_corr_vol_file)
+    n3_correction(st_corr_vol_file)
     calculate_m0(pd_vol)
 
 if __name__ == '__main__':
