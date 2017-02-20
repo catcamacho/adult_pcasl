9c3a7215f2ad0331d81898db23674729538d16db
catcamacho <m.catalina.camacho@gmail.com>
2017-02-19 17:16:24 -0800

    pipeline testing

--- quality_control.py
+++ quality_control.py
@@ -1,25 +1,30 @@
-import os
-import numpy as np
-import nipy as ni
-import nipype as npe
-import nibabel as nib
-from glob import glob
 
-def plot_hist(brain_volume):
-    """
-  	Plots a histogram of the inputed volume's voxel values.
-    """
-    pass
-	
-def save_slices_overlay(anat_volume,overlay_volume):
-    """
-  	Saves a slice by slice view of the overlay volume overlaid on the anat brain.
-  	This is helpful for checking subject-specific masking, group-wise T-maps, etc.
-    """
-	pass
-	
-def plot_groups(group_list,volume_file):
-    """
-  	Creates a scatterplot of all mean whole-brain CBFs to visually inspect outliers.
-    """
-	pass
+# coding: utf-8
+
+# In[3]:
+
+get_ipython().magic(u'matplotlib')
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+
+# In[13]:
+
+def plot_slice(fname, offset, vmin, vmax):
+    # Load the image and collect the data orientation information
+    img = nib.load(fname)
+    data = img.get_data()
+    aff = img.affine
+    # Find the center of the brain matrix
+    ctr = np.dot(np.linalg.inv(aff), [0, 0, 0, 1])[:3]
+    # Plot the data
+    plt.imshow(np.rot90(data[:, :,ctr[2] + offset]), cmap="viridis", vmin=vmin, vmax=vmax)
+    plt.gca().set_axis_off()
+
+
+# In[14]:
+
+plot_slice("/Users/catcamacho/Documents/asl_practice/ko/wrcbf_gm.nii.gz",1,0,200)
+
